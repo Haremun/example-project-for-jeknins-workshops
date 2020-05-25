6857e8ab96d25294482114e2a5569d346461b00f
Kamil <kamilbieg96@gmail.com>
2020-05-25 11:21:54 +0200

    part 2

--- src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
+++ src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
@@ -11,5 +11,6 @@ class Example {
   public static void main(String[] args) {
     System.out.println("Hello world");  
 	System.out.println("The Secret Cow Level");
+	System.out.println("Cow level doesn't exists");
   }
 }
